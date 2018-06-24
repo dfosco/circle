3bfcedf0b433250505014541c8a3c2085ba5d96e
Daniel Fosco <danielfosco@gmail.com>
2018-06-24 17:32:47 +0200

    Initial vue mvp

--- index.js
+++ index.js
@@ -1,6 +1,40 @@
 var app = new Vue({
   el: '#app',
-  data: {
-    message: 'assets/naruto_avatar.png'
+
+  data() {
+    return {
+      gradient: 'bg-bi',
+      url: 'https://i.imgur.com/Z1LPMoZ.png',
+      picked: 'rainbow'
+    }
+  },
+
+  components: {
+    'picture-input': PictureInput
+  },
+
+  methods: {
+    printPng: function() {
+      var node = document.getElementById('pride-avatar');
+      domtoimage.toPng(node)
+        .then(function (dataUrl) {
+          var link = document.createElement('a');
+          link.download = 'my-node.png';
+          link.href = dataUrl;
+          link.click();
+        })
+        .catch(function (error) {
+            console.error('oops, something went wrong!', error);
+        });
+    },
+    onChange (image) {
+      console.log('New picture selected!')
+      if (image) {
+        console.log('Picture loaded.')
+        this.image = image
+      } else {
+        console.log('FileReader API not supported: use the <form>, Luke!')
+      }
+    }
   }
 })
